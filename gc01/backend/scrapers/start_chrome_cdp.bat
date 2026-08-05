2a12035711a5a5493a0e09d3e498c32499c59727
yfy1115955678-design <yfy1115955678@gmail.com>
2026-08-05 09:27:18 +0800

    Delete gc01 directory

--- gc01/backend/scrapers/start_chrome_cdp.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-start "" "C:\Program Files\Google\Chrome\Application\chrome.exe" --remote-debugging-port=9222 --user-data-dir="C:\Users\ASUS\AppData\Local\Google\Chrome\User Data" --disable-blink-features=AutomationControlled --no-first-run --no-default-browser-check
-echo Chrome started with CDP on port 9222
